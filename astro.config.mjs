26a0d7a2d5913c8da152f1966f9b412b619c8aec
Patrick Jiang <patjiang04@gmail.com>
2026-03-19 13:10:29 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://amhd41.github.io',
-  base: '/your-lab-site',
+  site: 'https://patjiang.github.io',
+  base: '/StructureSystemAI.github.io/',
 });
